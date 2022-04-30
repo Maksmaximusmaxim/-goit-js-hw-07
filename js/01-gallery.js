1736e84b4cc43907ade33d6a67c17c2513b863ee
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-04-30 09:32:32 +0300

    dnwfkwrj

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const div = document.querySelector(`.gallery`);
+
+
+const crEl = galleryItems.map(galleryItem => 
+`<div class="gallery__item">
+<a class="gallery__link"  href="${galleryItem.original}"  >
+  <img
+    class="gallery__image"
+    src="${galleryItem.preview}"
+    data-source="${galleryItem.original}"
+    alt="${galleryItem.description}"
+  />
+</a>
+</div>`
+
+).join(" ");
+div.insertAdjacentHTML('afterbegin', crEl)
